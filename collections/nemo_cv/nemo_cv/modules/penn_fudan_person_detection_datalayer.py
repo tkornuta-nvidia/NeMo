ea9a1aa2ba284bde81c88c2a7a10fd2f8bb4e630
Tomasz Kornuta <tkornuta@nvidia.com>
2019-11-15 19:17:12 -0800

    wip on datalayers

---
+++ collections/nemo_cv/nemo_cv/modules/penn_fudan_person_detection_datalayer.py
@@ -0,0 +1,175 @@
+# Copyright (C) NVIDIA. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__author__ = "Tomasz Kornuta"
+
+import os
+import numpy as np
+from PIL import Image
+
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+from nemo.backends.pytorch.nm import DataLayerNM
+
+
+class PennFudanPedestrianDataset(Dataset):
+    """
+    Dataset containing images of pedestrians for object detection and
+    image segmentation (dense prediction).
+    """
+
+    def __init__(self, data_folder="~/data/PennFudanPed"):
+        """
+        Dataset constructor.
+        Args:
+            data_folder: path to the folder with data, can be relative to user.
+
+        """
+        # Get absolute path.
+        self.abs_data_folder = os.path.expanduser(data_folder)
+
+        # Load and sort all the image files.
+        self.imgs = list(
+            sorted(os.listdir(os.path.join(self.abs_data_folder, "PNGImages"))))
+        self.masks = list(
+            sorted(os.listdir(os.path.join(self.abs_data_folder, "PedMasks"))))
+
+        # Image transforms - to tensor.
+        self.transforms = transforms.Compose([transforms.ToTensor()])
+
+    def __getitem__(self, idx):
+        """
+        Returns a single sample, consisting of:
+            - index of a sample
+            - image_id (Int64Tensor[1]): an image identifier. It should be
+            unique between all the images in the dataset, and is used during
+            evaluation
+            - image: a PIL Image of size (H, W)
+            - masks (UInt8Tensor[N, H, W]): The segmentation masks
+            for each one of the objects
+            - boxes (FloatTensor[N, 4]): the coordinates of the N bounding
+            boxes in [x0, y0, x1, y1] format, ranging from 0 to W and 0 to H
+            - labels (Int64Tensor[N]): the label for each bounding box
+            - area (Tensor[N]): The area of the bounding box. This is used
+            during evaluation with the COCO metric, to separate the metric
+            scores between small, medium and large boxes.
+            - iscrowd (UInt8Tensor[N]): instances with iscrowd=True will be
+            ignored during evaluation.
+        """
+
+        # Load given image and  mask.
+        img_path = os.path.join(self.abs_data_folder,
+                                "PNGImages", self.imgs[idx])
+        mask_path = os.path.join(self.abs_data_folder,
+                                 "PedMasks", self.masks[idx])
+
+        # Conver image to RGB.
+        img = Image.open(img_path).convert("RGB")
+        img = self.transforms(img)
+
+        # Each color in mask corresponds to a different instance,
+        # with 0 being background.
+        mask = Image.open(mask_path)
+        mask = np.array(mask)
+
+        # Instances are encoded as different colors.
+        obj_ids = np.unique(mask)
+        # First id is the background, so remove it.
+        obj_ids = obj_ids[1:]
+
+        # Split the color-encoded mask into a set of binary masks.
+        masks = mask == obj_ids[:, None, None]
+
+        # get bounding box coordinates for each mask.
+        num_objs = len(obj_ids)
+        boxes = []
+        for i in range(num_objs):
+            pos = np.where(masks[i])
+            xmin = np.min(pos[1])
+            xmax = np.max(pos[1])
+            ymin = np.min(pos[0])
+            ymax = np.max(pos[0])
+            boxes.append([xmin, ymin, xmax, ymax])
+
+        # Change to tensors.
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        labels = torch.ones((num_objs,), dtype=torch.int64)
+        masks = torch.as_tensor(masks, dtype=torch.uint8)
+
+        image_id = torch.tensor([idx])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        # suppose all instances are not crowd
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+
+        return idx, image_id, img, boxes, labels, masks, area, iscrowd
+
+    def __len__(self):
+        return len(self.imgs)
+
+
+class PennFudanDataLayer(DataLayerNM):
+    """Wrapper around the PennFudan dataset.
+    """
+
+    @staticmethod
+    def create_ports():
+        """
+        Creates definitions of input and output ports.
+        """
+        input_ports = {}
+        output_ports = {
+            "indices": NeuralType({0, AxisType(Batch)}),
+            "images": NeuralType({0: AxisType(BatchTag),
+                                  1: AxisType(ChannelTag, 3),
+                                  2: AxisType(HeightTag),
+                                  3: AxisType(WidthTag)}),
+            "targets": NeuralType({0: AxisType(BatchTag)})
+        }
+        return input_ports, output_ports
+
+    def __init__(
+        self,
+        batch_size,
+        shuffle=True
+    ):
+        """
+        Initializes the datalayer.
+
+        Args:
+            batch_size: size of batch
+            data_folder: path to the folder with data, can be relative to user.
+            train: use train or test splits
+            shuffle: shuffle data(True by default)
+        """
+        # Passing the default params to base init call.
+        DataLayerNM.__init__(self)
+
+        self._batch_size = batch_size
+        self._train = train
+        self._shuffle = shuffle
+
+        self._dataset = PennFudanPedestrianDataset()
+
+    def __len__(self):
+        return len(self._dataset)
+
+    @property
+    def dataset(self):
+        return self._dataset
+
+    @property
+    def data_iterator(self):
+        return None
