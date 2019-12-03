204430161a15dad4715b0abee688bb3a78bc44a9
Tomasz Kornuta <tkornuta@nvidia.com>
2019-12-02 17:35:13 -0800

    mikro wip, still far away from destination :]
    Signed-off-by: Tomasz Kornuta <tkornuta@nvidia.com>

--- collections/nemo_cv/nemo_cv/modules/faster_rcnn.py
+++ collections/nemo_cv/nemo_cv/modules/faster_rcnn.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+# https://github.com/jwyang/faster-rcnn.pytorch/tree/pytorch-1.0
+
 __author__ = "Tomasz Kornuta"
 
 from torchvision.ops import misc as misc_nn_ops
